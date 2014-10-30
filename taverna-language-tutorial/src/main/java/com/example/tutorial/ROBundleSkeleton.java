3b42042a74ee3df2141464b574105f48ff2574ef
Alan R Williams <alan.r.williams@manchester.ac.uk>
2014-10-30 11:24:41 +0100

    Skeleton for ROBundle tutorial

---
+++ taverna-language-tutorial/src/main/java/com/example/tutorial/ROBundleSkeleton.java
@@ -0,0 +1,22 @@
+package com.example.tutorial;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.OutputStream;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.purl.wf4ever.robundle.Bundle;
+import org.purl.wf4ever.robundle.Bundles;
+
+import uk.org.taverna.databundle.DataBundles;
+
+public class ROBundleSkeleton {
+
+	public static void main(String[] args) throws Exception {
+	}
+}
